5f119fa36bee8cf0c9f245b44e6c8aa2fa42ed91
PhamPhuc09 <pphuc9122002@gmail.com>
2024-10-06 11:51:03 +0700

    feature[config] add header Authorization

--- src/main/java/org/graduate/shoefastbe/ShoeFastBeApplication.java
+++ src/main/java/org/graduate/shoefastbe/ShoeFastBeApplication.java
@@ -1,13 +1,27 @@
 package org.graduate.shoefastbe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		scheme = "bearer"
+)
+@OpenAPIDefinition(info = @Info(title = "Library Apis",version = "V.1.0",description = "Documentation for Library Management apis")
+		,security = {@SecurityRequirement(name = "bearerAuth")})
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableScheduling
+
 public class ShoeFastBeApplication {
 
 	public static void main(String[] args) {
