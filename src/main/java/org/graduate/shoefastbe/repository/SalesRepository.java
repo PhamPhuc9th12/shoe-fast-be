8238d3ab1de32ef4a03f565120fbc48fe4d75121
PhamPhuc09 <pphuc9122002@gmail.com>
2024-10-09 21:31:10 +0700

    feature[account] add sales

---
+++ src/main/java/org/graduate/shoefastbe/repository/SalesRepository.java
@@ -0,0 +1,13 @@
+package org.graduate.shoefastbe.repository;
+
+import org.graduate.shoefastbe.entity.SalesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+
+@Repository
+public interface SalesRepository extends JpaRepository<SalesEntity, Long> {
+    List<SalesEntity> findAllByIdIn(Collection<Long> ids);
+}
